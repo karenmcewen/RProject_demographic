12e0023c4cead3c1ec71114f94d4312bc4be82e2
Karen McEwen <karenmcewen.engineer@gmail.com>
2020-08-09 09:44:30 -0400

    more visualization with qplot

--- RDemo1.R
+++ RDemo1.R
@@ -129,4 +129,43 @@ head(mergedDF)
 
 mergedDF$Country<-NULL #remove extra country column
 head(mergedDF)
-str(mergedDF)
+str(mergedDF) #structure
+
+#------------------------- VISUALIZING with QPLOT ----------
+qplot(data=mergedDF,x=Internet.users,y=Birth.rate,
+      color=Region, size=I(5))
+
+#shapes - numbers 15-20 are filled shapes
+qplot(data=mergedDF,x=Internet.users,y=Birth.rate,
+      color=Region, size=I(5), shape=I(17))
+
+#transparency - alpha - to show overlapping points 
+#0(fully transparent) to 1(fully opaque)
+qplot(data=mergedDF,x=Internet.users,y=Birth.rate,
+      color=Region, size=I(5), shape=I(17), alpha=I(0.6))
+
+#Add Title - main
+qplot(data=mergedDF,x=Internet.users,y=Birth.rate,
+      color=Region, size=I(5), shape=I(17), 
+      alpha=I(0.4), main="Birth Rate vs Internet Usage")
+
+#------------------------- HOMEWORK ---------
+#You are required to produce a scatterplot depicting 
+#Life Expectancy (y-axis) and
+#Fertility Rate (x-axis) statistics by Country.
+#The scatterplot needs to also be categorised 
+#by Countries’ Regions.
+
+#You have been supplied with data for 2 years: 1960 and 2013 and you are required
+#to produce a visualisation for each of these years.
+#Some data has been provided in a csv file, 
+#some - in R vectors. The csv file contains
+#combined data for both years. 
+#All data manipulations have to be performed in R 
+#(not in excel) because this project may be audited at a later stage.
+#You have also been requested to provide insights 
+#into how the two periods compare. 
+
+#1)download the data into the correct folder
+#2)upload the data into R
+stats2<-read.csv(file.choose())
